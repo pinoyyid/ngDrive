03529e82618f8b558ef61f348986b8799f528e0f
Roy Smith <roy.smith.esq@gmail.com>
2015-12-05 13:12:18 +0000

    grunt and npm tidy up

--- src/0_module_def.js
+++ src/0_module_def.js
@@ -1,1 +1,2 @@
 angular.module('ngm.NgGapi', []);
+//# sourceMappingURL=0_module_def.js.map
--- src/http_s.js
+++ src/http_s.js
@@ -1,3 +1,4 @@
+/// <reference path="../nggapi_ts_declaration_files/drive_interfaces.d.ts"/>
 'use strict';
 var NgGapi;
 (function (NgGapi) {
@@ -197,3 +198,4 @@ var NgGapi;
 })(NgGapi || (NgGapi = {}));
 angular.module('ngm.NgGapi')
     .service('HttpService', NgGapi.HttpService);
+//# sourceMappingURL=http_s.js.map
